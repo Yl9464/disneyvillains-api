8a02b30f49b07f7c8c0197b941cd45984a401f55
Yl9464 <86930915+Yl9464@users.noreply.github.com>
2021-12-28 16:52:35 -0500

    getAllVillains test passing

--- tests/controllers/villains.test.js
+++ tests/controllers/villains.test.js
@@ -3,18 +3,25 @@ const sinon = require('sinon')
 const chai = require('chai')
 const sinonChai = require('sinon-chai')
 const { describe, it } = require('mocha')
-const {getAllVillains, getVillainBySlug, newVillain} = require('../../controllers/villains')
+const { getAllVillains, getVillainBySlug, newVillain } = require('../../controllers/villains')
+const { response } = require('express')
+const models = require('../../models')
+const { villainsList } = require('./mocks')
 
 chai.use(sinonChai)
 const { expect } = chai
 
 describe('Controllers - Villains', () => {
   describe('getAllVillains', () => {
-    describe('getVillainBySlug', () => {
-      describe('newVillain', () => {
+    it('retrieve a list of villians from the db and call response.send with that list', async () => {
+      const stubbedFindAll = sinon.stub(models.villains, 'findAll').returns(villainsList) // substitue for the real findAll(). First param name of file being called and second param is the function being stubbed
+      const stubbedSend = sinon.stub()
+      const response = { send: stubbedSend }
 
-      })
+      await getAllVillains({}, response)
+
+      expect(stubbedFindAll).to.have.callCount(1)
+      expect(stubbedSend).to.have.been.calledWith(villainsList)
     })
   })
 })
-
